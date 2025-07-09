c9e3ae29b9247545e367880718e2933046ba4a79
tumer311 <etumer31148@gmail.com>
2025-07-09 13:17:55 +0300

    no change today, only tried an example about state management. It is actually about memory management. Mutating or changing an existing state is bad, creating and returning the newly created state is good.

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,10 @@ const counterReducer = (state = initialState, action) => {
   }
 
   if (action.type === "increase") {
-    return { counter: state.counter + action.amount, showCounter: state.showCounter };
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
   }
 
   if (action.type === "decrement") {
@@ -18,8 +21,8 @@ const counterReducer = (state = initialState, action) => {
   if (action.type === "toggle") {
     return {
       showCounter: !state.showCounter,
-      counter: state.counter
-    }
+      counter: state.counter,
+    };
   }
 
   return state;
